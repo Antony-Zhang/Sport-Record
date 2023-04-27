4626c64f7bbf5cbf868e581288ac583681ac349f
FIshead_East <ytzd2696@foxmail.com>
2023-04-27 14:08:37 +0800

    Initial Commit

---
+++ Sport Record/Sport_RecordApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sport_RecordApp.swift
+//  Sport Record
+//
+//  Created by EastOS on 2023/4/27.
+//
+
+import SwiftUI
+
+@main
+struct Sport_RecordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
