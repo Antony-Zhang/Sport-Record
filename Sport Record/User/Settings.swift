cd030d9aae86123b02c7a0ee91a82fc2d23e6c6b
Fishead_East <ytzd2696@foxmail.com>
2023-05-13 17:52:24 +0800

    重构用户设置界面

--- Sport Record/User/Settings.swift
+++ Sport Record/User/Settings.swift
@@ -12,57 +12,99 @@ struct Settings : View{
      运动单位:双重选择——1⃣️时间上
      响铃设置:
      */
-    let UnitOptions = 0...30
-    @State var isEditMode = false
+    
+    // ⚠️该变量未配好,会导致Picker呈现失败、XCode崩溃
     @EnvironmentObject var userSettings: UserSettings
+    
+    @State var isEditMode = true
+    // 选择选项的[索引]
+    @State private var unitsSelect = 0
+    @State private var ringSelect = 0
+    @State private var planSelect = 0
+    
+    let UnitsOptions = Array(1...30)
+    let RingOptions = ["Music1","Music2","Music3"]
+    let PlanOptions = Array(1...3)
     
     var body: some View{
         NavigationView {
-            VStack(alignment: .center){
-                VStack{
-                    Text("运动单位").font(.title).fontWeight(.bold)
-                    if(isEditMode){
-                        
-                    }else{
-                        
+            Form {
+                Section(header: Text("单次运动的单位")) {
+                    Picker(selection: $unitsSelect, label: Text("运动单位(min)")) {
+                        ForEach(0 ..< UnitsOptions.count) { index in
+                            Text(String(self.UnitsOptions[index])).tag(index)
+                        }
                     }
-                }.padding()
-                VStack{
-                    Text("响铃设置").font(.title).fontWeight(.bold)
-                    if(isEditMode){
-                        
-                    }else{
-                        
-                    }
-                }.padding()
-                VStack{
-                    Text("任务计划").font(.title).fontWeight(.bold)
-                    if(isEditMode){
-                        
-                    }else{
-                        
-                    }
-                }.padding()
-                
-                if(!isEditMode){
-                    Button("修改设置") {
-                        isEditMode = true;
-                    }.padding(.top).buttonStyle(BlueRoundedButton())  // 使用自定义的样式
-                }else{
-                    HStack{
-                        Button("取消") {
-                            isEditMode = false ;
-                        }.padding(.trailing).buttonStyle(RedRoundedButton())
-                        Button("确定") {
-                            
-                            isEditMode = false;
-                        }.padding(.leading).buttonStyle(BlueRoundedButton())
-                    }.padding(.top)
                 }
-            }.position(x:200,y:220)
+                Section(header: Text("运动结束后的铃声")) {
+                    Picker(selection: $unitsSelect, label: Text("响铃设置")) {
+                        ForEach(RingOptions,id: \.self){    // id为自身
+                            Text($0)    // $0指代闭包中的第一个参数,此处就是RingOptions的每一个元素
+                        }
+                    }
+                }
+                Section(header: Text("前后两次运动的间隔")) {
+                    Picker(selection: $unitsSelect, label: Text("任务计划")) {
+                        ForEach(0 ..< PlanOptions.count) {
+                            op in Text("\(op)")
+                        }
+                    }
+                }
+                
+            }//.position(x:200,y:220)
                 .navigationTitle("应用设置")
                 // 视图消失后保存设置
                 .onDisappear{userSettings.saveSettings()}
+                
+//                VStack{
+//                    Text("运动单位").font(.title)
+//                        .fontWeight(.bold)
+//                        .padding()
+//                    if(isEditMode){
+//                        Picker("Select a number", selection: $unitsSelect) {
+//                            ForEach(UnitsOptions, id: \.self) {
+//                                op in Text("\(op)")
+//                            }
+//                        }
+//                        .pickerStyle(DefaultPickerStyle())
+//                    }else{
+//
+//                    }
+//                }.padding()
+//                VStack{
+//                    Text("响铃设置").font(.title)
+//                        .fontWeight(.bold)
+//                    if(isEditMode){
+//
+//                    }else{
+//
+//                    }
+//                }.padding()
+//                VStack{
+//                    Text("任务计划").font(.title)
+//                        .fontWeight(.bold)
+//                    if(isEditMode){
+//
+//                    }else{
+//
+//                    }
+//                }.padding()
+//
+//                if(!isEditMode){
+//                    Button("修改设置") {
+//                        isEditMode = true;
+//                    }.padding(.top).buttonStyle(BlueRoundedButton())  // 使用自定义的样式
+//                }else{
+//                    HStack{
+//                        Button("取消") {
+//                            isEditMode = false ;
+//                        }.padding(.trailing).buttonStyle(RedRoundedButton())
+//                        Button("确定") {
+//
+//                            isEditMode = false;
+//                        }.padding(.leading).buttonStyle(BlueRoundedButton())
+//                    }.padding(.top)
+//                }
         }
     }
 }
